1a85d1004a39577ac8ff224694749f589826f53e
kenshiro7110 <kenshiro7110@gmail.com>
2022-05-30 09:30:33 +0900

    フォルダ名変更に伴うコード変更

--- W2メイン/MainDisplay.js
+++ W2メイン/MainDisplay.js
@@ -1,3 +1,3 @@
 function popup(){
-window.open('../W3/DayDisplay.html', null ,'top=200,left=200,width=600,height=600');
+window.open('../W3日別/DayDisplay.html', null ,'top=200,left=200,width=600,height=600');
 }
